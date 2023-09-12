68ee31f62d08fc287b45cf495fff32f6b9619f4b
Manoj Ahirwar <manojahirwar11@gmail.com>
2023-09-12 23:39:51 +0530

    change

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <CustomerSupport />
+        {/* <CustomerSupport /> */}
         <link
           href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700&display=swap"
           rel="stylesheet"
